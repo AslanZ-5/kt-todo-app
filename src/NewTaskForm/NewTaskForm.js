31da403f445358a85d25f023ae8c27fd7e935b52
Aslan <asl.zurabov@gmail.com>
2023-01-08 11:38:33 +0300

    task input forms switched to functional components with hooks

--- src/NewTaskForm/NewTaskForm.js
+++ src/NewTaskForm/NewTaskForm.js
@@ -1,61 +1,55 @@
-import React, { Component } from "react";
+import React, { useState, useCallback } from "react";
 import PropTypes from "prop-types";
 
-class NewTaskForm extends Component {
-  state = {
+function NewTaskForm({ AddItem }) {
+  const [inputData, setInputData] = useState({
     val: "",
     min: "",
     sec: "",
-  };
+  });
+  const onChangeInput = useCallback((e) => {
+    const newInputData = { ...inputData, [e.target.name]: e.target.value };
+    setInputData(newInputData);
+  });
+  const { val, min, sec } = inputData;
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
 
-  onChangeInput = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  render() {
-    const { val, min, sec } = this.state;
-    const { AddItem } = this.props;
-    return (
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-
-          AddItem(val, min, sec);
-          this.setState({
-            val: "",
-            min: "",
-            sec: "",
-          });
-        }}
-        className="new-todo-form"
-      >
-        <input
-          value={val}
-          name="val"
-          onChange={this.onChangeInput}
-          className="new-todo"
-          placeholder="What needs to be done?"
-        />
-        <input
-          value={min}
-          name="min"
-          onChange={this.onChangeInput}
-          className="new-todo-form__timer"
-          placeholder="Min"
-        />
-        <input
-          value={sec}
-          name="sec"
-          onChange={this.onChangeInput}
-          className="new-todo-form__timer"
-          placeholder="Sec"
-        />
-        <input type="submit" hidden />
-      </form>
-    );
-  }
+        AddItem(val, min, sec);
+        setInputData({
+          val: "",
+          min: "",
+          sec: "",
+        });
+      }}
+      className="new-todo-form"
+    >
+      <input
+        value={val}
+        name="val"
+        onChange={onChangeInput}
+        className="new-todo"
+        placeholder="What needs to be done?"
+      />
+      <input
+        value={min}
+        name="min"
+        onChange={onChangeInput}
+        className="new-todo-form__timer"
+        placeholder="Min"
+      />
+      <input
+        value={sec}
+        name="sec"
+        onChange={onChangeInput}
+        className="new-todo-form__timer"
+        placeholder="Sec"
+      />
+      <input type="submit" hidden />
+    </form>
+  );
 }
 
 export default NewTaskForm;
